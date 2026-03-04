b98a28963c75440074de9d40eb36941d926b7f30
Akash Verma <vermaakash04012002@gmail.com>
2026-03-05 01:55:49 +0530

    change vite

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/js/theme.js'
             ],
+            buildDirectory: 'build',
             refresh: true,
         }),
     ],
